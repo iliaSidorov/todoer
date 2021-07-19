a0b3e7275525a4d1f220f336cef7d30550f526a2
Ilya Sidorov <ilyasidorov114@gmail.com>
2021-07-19 20:46:04 +0300

    Some more logic

--- app/src/main/java/com/example/todoer/Todo.kt
+++ app/src/main/java/com/example/todoer/Todo.kt
@@ -2,5 +2,5 @@ package com.example.todoer
 
 data class Todo(
     val title: String,
-    val isChecked: Boolean = false
+    var isChecked: Boolean = false
 )
--- app/src/main/java/com/example/todoer/TodoAdapter.kt
+++ app/src/main/java/com/example/todoer/TodoAdapter.kt
@@ -22,15 +22,39 @@ class TodoAdapter (private val todos: MutableList<Todo>) : RecyclerView.Adapter<
         )
     }
 
+    fun addTodo(todo: Todo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    fun deleteDoneTodos() {
+        todos.removeAll { todo ->
+            todo.isChecked
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if (isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
         holder.itemView.apply {
             tvTodoTitle.text = curTodo.title
             cbDone.isChecked = curTodo.isChecked
+            toggleStrikeThrough(tvTodoTitle, curTodo.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked)
+                curTodo.isChecked = !curTodo.isChecked
+            }
         }
     }
 
-    override fun getItemCount(): Int {
-        return todos.size
-    }
+    override fun getItemCount() = todos.size
+
 }
